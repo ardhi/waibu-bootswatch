6aeae2865792c6ad41b50464ce34c5414484ed28
Ardhi Lukianto <ardhi@lukianto.com>
2024-10-07 21:14:18 +0700

    Updates

--- bajo/hook/waibu-mpa@after-build-page.js
+++ bajo/hook/waibu-mpa@after-build-page.js
@@ -1,5 +1,5 @@
-async function waibuMpaAfterBuildPage ({ $, theme, reply }) {
-  if (reply.request.darkMode && theme.framework === 'bootstrap') $('html').attr('data-bs-theme', 'dark')
+async function waibuMpaAfterBuildPage ({ $, theme, req }) {
+  if (req.darkMode && theme.framework === 'bootstrap') $('html').attr('data-bs-theme', 'dark')
 }
 
 export default waibuMpaAfterBuildPage
