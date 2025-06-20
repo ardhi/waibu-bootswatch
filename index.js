dde57934515aabf4c93520a8003f3d9d814012a0
Ardhi Lukianto <ardhi@lukianto.com>
2025-06-20 22:05:42 +0700

    Updates

---
+++ index.js
@@ -0,0 +1,18 @@
+async function factory (pkgName) {
+  const me = this
+
+  return class WaibuBootswatch extends this.lib.BajoPlugin {
+    constructor () {
+      super(pkgName, me.app)
+      this.alias = 'wbw'
+      this.dependencies = ['waibu-mpa', 'waibu-bootstrap']
+      this.config = {
+        waibu: {
+          prefix: 'bootswatch'
+        }
+      }
+    }
+  }
+}
+
+export default factory
