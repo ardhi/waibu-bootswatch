20b0a2d9f68759d6f37d5accc397650185c5d7e0
ardhi <ardhi.lukianto@gmail.com>
2026-02-09 01:11:08 +0700

    Updates

--- extend/bajo/hook/waibu-mpa@after-build-page.js
+++ extend/bajo/hook/waibu-mpa@after-build-page.js
@@ -1,5 +1,4 @@
 async function waibuMpaAfterBuildPage ({ $, theme, req }) {
-  console.log(theme)
   if (req.darkMode && theme.framework === 'bootstrap') $('body').attr('data-bs-theme', 'dark')
 }
 
--- extend/waibuMpa/theme.js
+++ extend/waibuMpa/theme.js
@@ -2,7 +2,7 @@ const types = ['cerulean', 'cosmo', 'cyborg', 'darkly', 'flatly', 'journal', 'li
   'lumen', 'lux', 'materia', 'minty', 'morph', 'pulse', 'quartz', 'sandstone', 'simplex',
   'sketchy', 'slate', 'solar', 'spacelab', 'superhero', 'united', 'vapor', 'yeti', 'zephyr']
 
-async function theme (ctx) {
+async function theme () {
   return types.map(t => {
     const framework = 'bootstrap'
     const css = `waibuBootswatch.virtual:/bootswatch/${t}/bootstrap.min.css`
