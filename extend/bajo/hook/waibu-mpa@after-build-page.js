0a5ca2a788c028cc4765fce319b7a230252a21e0
ardhi <ardhi.lukianto@gmail.com>
2026-04-07 21:59:55 +0700

    Updates

--- extend/bajo/hook/waibu-mpa@after-build-page.js
+++
@@ -1,5 +0,0 @@
-async function waibuMpaAfterBuildPage ({ $, theme, req }) {
-  if (req.darkMode && theme.framework === 'bootstrap') $('body').attr('data-bs-theme', 'dark')
-}
-
-export default waibuMpaAfterBuildPage
